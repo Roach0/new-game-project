e625d1db8f29b8ccdc40e40f55ea1a3da9b3c9cb
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2026-05-20 17:41:29 -0500

    gg

--- card_queue.gd
+++ card_queue.gd
@@ -1,11 +1,17 @@
+class_name CardQueue
 extends HBoxContainer
 
 const MAX_CARDS = 6
 var card_queue: Array[Card] = []
-var full = false
+
+
+
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
 	pass
+
+func is_full() -> bool: 
+	return card_queue.size() >= MAX_CARDS
--- deck.gd
+++ deck.gd
@@ -1,8 +1,10 @@
+class_name Deck
 extends AspectRatioContainer
 
 @export var starting_cards: Array[Card] = []
 @export var queue: NodePath
 
+var queue_node: CardQueue
 var cards: Array[Card] = []
 var discards: Array[Card] = []
 
@@ -13,11 +15,8 @@ func _process(delta: float) -> void:
 	pass
 
 func card_draw():
-	if cards.is_empty():
-		return {
-			"name": "cards_empty",
-			"desc": "Deck is empty"
-		}
+	if cards.is_empty() || queue_node.is_full():
+		return
 	var card = cards.pop_front()
 	discards.append(card)
 	return card
@@ -25,10 +24,9 @@ func card_draw():
 func build_deck():
 	for card in cards:
 		cards.append(card.duplicate())
+		cards.shuffle()
 		
 
 
 func _on_button_pressed() -> void:
-	if queue.full == true:
-		pass
-	
+	card_draw()
