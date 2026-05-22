ba7d1d2a04869508d09e2aa3cb8dddbcba633abb
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2026-05-21 21:18:42 -0500

    stuff

--- encounter.gd
+++ encounter.gd
@@ -1,13 +1,1 @@
-extends HBoxContainer
-
-var grid : Array[Array] = [[],[]]
-
-@export var entities : Array[EncounterEntity] = []
-
-func _ready() -> void:
-	pass # Replace with function body.
-func _process(delta: float) -> void:
-	pass
-
-func grid_setup():
-	entities.shuffle()
+extends AspectRatioContainer
