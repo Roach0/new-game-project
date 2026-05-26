38038b519283d3b03cde00f32fd36ab20e6c261b
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2026-05-25 21:33:09 -0500

    f
    f

--- components/encounter/encounter.gd
+++ components/encounter/encounter.gd
@@ -1,2 +1,16 @@
 extends VBoxContainer
 class_name Encounter
+
+@onready var encounter_characters : Array[CharacterResource]
+@onready var top_row = $TopRow
+@onready var bot_row = $BotRow
+
+var character_slot = preload("res://components/character_slot/character_slot.tscn")
+
+func assemble_encounter(encounter:EncounterResource) -> void:
+	for c in encounter.characters:
+		encounter_characters.append(c)
+	encounter_characters.shuffle()
+	for c in encounter_characters:
+		if top_row.get_child_count() < 5:
+			top_row.add_child(character_slot)
