5f619badba29d329441ed835922ff64eac760930
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2026-05-27 15:16:18 -0500

    button visible

--- components/queue/card_queue.gd
+++ components/queue/card_queue.gd
@@ -8,6 +8,12 @@ extends MarginContainer
 	$VBoxContainer/CardQueue/Slot3,
 	$VBoxContainer/CardQueue/Slot4]
 
+@onready var button0: Button = $VBoxContainer/ButtonQueue/Button0
+@onready var button1: Button = $VBoxContainer/ButtonQueue/Button1
+@onready var button2: Button = $VBoxContainer/ButtonQueue/Button2
+@onready var button3: Button = $VBoxContainer/ButtonQueue/Button3
+@onready var button4: Button = $VBoxContainer/ButtonQueue/Button4
+
 signal queue_is_full
 signal discard
 
@@ -18,8 +24,13 @@ func _ready() -> void:
 # queries
 func open_slots() -> Array:
 	return slots.filter(func(s): return s.is_empty())
-func next_open_slot() -> QueueSlot:
-	return open_slots().front()
+
+func next_open_slot() -> Array:
+	var next_slot = open_slots().front()
+	var next_slot_number = next_slot.name.lstrip("Slot")
+	var next_slot_button = get_node("VBoxContainer/ButtonQueue/Button" + next_slot_number)
+	return [next_slot, next_slot_button]
+
 func is_full() -> bool:
 	return open_slots().is_empty()
 
@@ -28,10 +39,31 @@ func add_card(card: CardResource) -> void:
 	if is_full():
 		queue_is_full.emit()
 		return
-	var slot = next_open_slot()
+	var slot = next_open_slot()[0]
+	var button = next_open_slot()[1]
 	if slot:
 		slot.assign(card)
+		button.modulate.a = 1.0
 
 # handlers
 func _on_slot_discard_request(card: CardResource) -> void:
 	discard.emit(card)
+
+func _on_button_0_pressed() -> void:
+	print()
+
+
+func _on_button_1_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_3_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_4_pressed() -> void:
+	pass # Replace with function body.
