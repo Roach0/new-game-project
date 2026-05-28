ce36847f1ac75d787947c966b1faa4a276ee8e57
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2026-05-28 17:14:05 -0500

    g
    g

--- resources/resource_classes/encounter_resource.gd
+++ resources/resource_classes/encounter_resource.gd
@@ -4,4 +4,4 @@ class_name EncounterResource
 @export var name:String = "encounter_name"
 @export var characters : Array[CharacterResource] = []
 
-@export var tags : Array[String] = [] #["hackable","container","enemy", etc...]
+@export var tags : Array[String] = []  # ["hackable","container","enemy", etc...]
