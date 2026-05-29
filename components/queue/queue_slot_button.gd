33ad6e19325a65659d5c6fc82dfc0a073a4a76cd
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2026-05-29 17:36:59 -0500

    f
    f

--- components/queue/queue_slot_button.gd
+++ components/queue/queue_slot_button.gd
@@ -4,16 +4,7 @@ extends Button
 @onready var triggerLabel: Label = $VBoxContainer/Trigger
 @onready var effectLabel: Label = $VBoxContainer/Effect
 
-var target: String = "":
-	set(value): target = value; targetLabel.text = value
-
-var trigger: String = "":
-	set(value): trigger = value; triggerLabel.text = value
-
-var effect: String = "":
-	set(value): effect = value; effectLabel.text = value
-
 func update(tar: String, tri: String, eff: String) -> void:
-	target = tar
-	trigger = tri
-	effect = eff
+	targetLabel.text = tar
+	triggerLabel.text = tri
+	effectLabel.text = eff
