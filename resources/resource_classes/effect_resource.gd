1748aabacf4d03d8c99a1a3aa35a3ea97c2af334
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2026-06-01 02:03:09 -0500

    d
    d

--- resources/resource_classes/effect_resource.gd
+++ resources/resource_classes/effect_resource.gd
@@ -1,16 +1,19 @@
 class_name EffectResource
 extends Resource
 
-# example: deal 3 damage
-
-@export var effect_description: String
+@export var effect_id: String
 @export var target: Target
+
+func _tick() -> void:
+	pass
 
 enum Target {
 	PLAYER = 0,
 	SELF = 1,
 	QUEUE_GROUP = 2,
 	QUEUE_POS = 3,
-	ENCOUNTER = 4,
-	# ENCOUNTER_GROUP
+	ENCOUNTER_GROUP = 4,
+	ENCOUNTER_POS = 5
 }
+
+var tags: Array[String] = []
