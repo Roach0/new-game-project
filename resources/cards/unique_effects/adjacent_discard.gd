ead2dd7a142992b1470f3a7e00c4a74d48b0737b
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2026-06-02 08:56:52 -0500

    fix

--- resources/cards/unique_effects/adjacent_discard.gd
+++ resources/cards/unique_effects/adjacent_discard.gd
@@ -1,8 +1,12 @@
 extends EffectResource
-class_name QueueLock
+class_name AdjacentDiscard
 var slot_targets:Array[QueueSlot] = []
 
 var queue_manager:CardQueue
 
 func tick(target: QueueSlot) -> void:
+	
+	queue_manager = target.get_parent().get_parent().get_parent()
+	
 	queue_manager.lock_queue_slot(target)
+	
--- resources/cards/unique_effects/queue_lock.gd
+++ resources/cards/unique_effects/queue_lock.gd
@@ -1,8 +1,9 @@
 extends EffectResource
-class_name AdjacentDiscard
+class_name QueueLock
 var slot_targets:Array[QueueSlot] = []
 
 var queue_manager:CardQueue
 
 func tick(target: QueueSlot) -> void:
+	queue_manager = target.get_parent().get_parent().get_parent()
 	queue_manager.lock_queue_slot(target)
